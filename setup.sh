6883da5411f856cf8cec23c952e221b05ef277a2
smobert <shobert2022@gmail.com>
2026-06-07 02:24:26 -0400

    updated setup shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm -rf ~/.config/hypr
+rm -rf ~/.config/kitty
+rm -rf ~/.config/nvim
+rm -rf ~/.config/waybar
+rm -rf ~/.config/wofi
+
 ln -s ~/.dotfiles/hypr ~/.config/hypr
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/nvim ~/.config/nvim
